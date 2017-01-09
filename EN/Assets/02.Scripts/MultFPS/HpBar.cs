7a4a09f7bc2fc2c89c7e0bc514e19566b465c027
DESKTOP-DMGA2LE\KTR_ASUS <ssmktr@naver.com>
2017-01-09 21:13:03 +0900

    no message

---
+++ EN/Assets/02.Scripts/MultFPS/HpBar.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HpBar : MonoBehaviour {
+
+    GUIStyle HpStyle;
+    GUIStyle BackStyle;
+    Combat _ComBat;
+
+	void Start () {
+        _ComBat = GetComponent<Combat>();
+	}
+
+    private void OnGUI()
+    {
+
+    }
+
+    void InitStyle()
+    {
+    }
+
+    Texture2D MakeTex(int width, int height, Color col)
+    {
+        Color[] pix = new Color[width * height];
+        for (int i = 0; i < pix.Length; ++i)
+        {
+            pix[i] = col;
+        }
+
+        Texture2D result = new Texture2D(width, height);
+        result.SetPixels(pix);
+        result.Apply();
+        return result;
+    }
+}
---
+++ EN/Assets/02.Scripts/MultFPS/PlayerMove.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour {
+
+    public GameObject BulletPrefab;
+
+    float MoveSpeed = 3f;
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    void Update () {
+
+        if (!isLocalPlayer)
+            return;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        transform.Translate(x * MoveSpeed * Time.deltaTime, 0, z * MoveSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+	}
+
+    [Command]
+    void CmdFire()
+    {
+        var Bullet = (GameObject)Instantiate(BulletPrefab, transform.position - transform.forward, Quaternion.identity);
+        Bullet.GetComponent<Rigidbody>().velocity = -transform.forward * 4;
+
+        NetworkServer.Spawn(Bullet);
+
+        Destroy(Bullet, 2f);
+    }
+}
