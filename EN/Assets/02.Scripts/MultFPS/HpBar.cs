ac5732c85f9ae85a089c810e58d2f1ba5a1895db
DESKTOP-DMGA2LE\KTR_ASUS <ssmktr@naver.com>
2017-01-09 21:25:26 +0900

    no message

--- EN/Assets/02.Scripts/MultFPS/HpBar.cs
+++ EN/Assets/02.Scripts/MultFPS/HpBar.cs
@@ -13,11 +13,32 @@ public class HpBar : MonoBehaviour {
 
     private void OnGUI()
     {
+        InitStyle();
 
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+
+        GUI.color = Color.gray;
+        GUI.backgroundColor = Color.grey;
+        GUI.Box(new Rect(pos.x - 26, Screen.height - pos.y + 20, Combat.MaxHp / 2, 7), ".", BackStyle);
+
+        GUI.color = Color.green;
+        GUI.backgroundColor = Color.green;
+        GUI.Box(new Rect(pos.x - 25, Screen.height - pos.y + 21, _ComBat.Hp / 2, 5), ".", HpStyle);
     }
 
     void InitStyle()
     {
+        if (HpStyle == null)
+        {
+            HpStyle = new GUIStyle(GUI.skin.box);
+            HpStyle.normal.background = MakeTex(2, 2, new Color(0f, 1f, 0f, 1f));
+        }
+
+        if (BackStyle == null)
+        {
+            BackStyle = new GUIStyle(GUI.skin.box);
+            BackStyle.normal.background = MakeTex(2, 2, new Color(0f, 0f, 0f, 1f));
+        }
     }
 
     Texture2D MakeTex(int width, int height, Color col)
